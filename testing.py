089f385cb54b84e7de33990b169e4750f6d5221a
Madskuz <112597537+Madskuz@users.noreply.github.com>
2022-12-06 15:38:52 -0500

    ok

---
+++ testing.py
@@ -0,0 +1,24 @@
+# Import the json and requests modules
+import json
+import requests
+
+# Set the API key for your Monday.com account
+api_key = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIwNTEwMjMyMywidWlkIjozNzEwMjA4OSwiaWFkIjoiMjAyMi0xMi0wNVQyMjoxNzo1OS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQzNjU4NTEsInJnbiI6InVzZTEifQ.u0OkluQ6Ks7JvSVByLv8sjhkmkTgEN7QkTEIFoGfsZ0"
+
+# Set the URL for the Monday.com API endpoint
+url = 'https://api.monday.com/v2/'
+
+# Set the parameters for the API request
+params = {'api_key': api_key}
+
+# Send the API request and retrieve the response
+response = requests.get(url, params=params)
+
+# Parse the JSON data from the response
+data = json.loads(response.text)
+
+# Convert the parsed data into a dictionary
+data = dict(data)
+
+# Print the dictionary
+print(data)
--- testmain.py
+++ testmain.py
@@ -1,8 +1,9 @@
 import requests
 import json
-from collections import Counter
-import pprint
 import pandas as pd
+from collections import defaultdict
+
+# Import core data
 
 apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIwNTEwMjMyMywidWlkIjozNzEwMjA4OSwiaWFkIjoiMjAyMi0xMi0wNVQyMjoxNzo1OS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQzNjU4NTEsInJnbiI6InVzZTEifQ.u0OkluQ6Ks7JvSVByLv8sjhkmkTgEN7QkTEIFoGfsZ0"
 apiUrl = "https://api.monday.com/v2/"
@@ -16,7 +17,12 @@ json_data = json.loads(requests.post(url=apiUrl, json=data, headers=headers).tex
 # source: https://stackoverflow.com/questions/68076059/normalize-monday-com-api-json-output-in-python
 data = [ [item['name']]+[c_v['text'] for c_v in item['column_values']] for item in json_data['data']['boards'][0]['items']]
 df = pd.DataFrame(data,columns=['Deals','Person','Status','Date'])
-df = df.reset_index()
+# print(df)
+
+
+###########################################################
+
+# General Statistics
 
 intial_dd_count = df['Status'].str.contains('Initial DD').sum()
 get_intro_count = df['Status'].str.contains('Get Intro').sum()
@@ -32,6 +38,122 @@ def general_overview_print():
 
 print(general_overview_print())
 
-groups = df.groupby("Person")
-mapping = {name: group["Deals"] for name, group in groups}
-print(mapping)
+
+###########################################################
+
+# Create a general overview of which deals are assigned to which person and the number of deals per person
+# Create dictionary that assigns deals to repsective individuals and print out this overview
+d = [dict(zip(["Deals", "Person"], item)) for item in data]
+
+dict_person_deals = defaultdict(list)
+
+for item in d:
+    dict_person_deals[item['Person']].append(item['Deals'])
+
+def count_len_dict(person):
+    """
+    a function that counts the number of values per key (the number of deals assigned to the person)
+    """
+    count = len(dict_person_deals.get(person))
+    return count
+
+# print(f"""
+# \n\033[4mIndividual Pipeline:\033[0m
+# Hrishi's deals:{dict_list.get('Hrishi')}
+# Total deals: {count_len_dict('Hrishi')}\n
+# Mads's deals:{dict_list.get('mpedersen1@babson.edu')}
+# Total deals: {count_len_dict('mpedersen1@babson.edu')}\n""")
+
+
+###########################################################
+
+# create a dictionary that contains the person, the deals and the status of these deals and print out this overview
+d1 = [dict(zip(["Deals", "Person","Status"], item)) for item in data]
+
+dict_status_deals = defaultdict(list)
+
+# Group by status by adding the deals (value) to the status (key)
+for item in d1:
+    dict_status_deals[item['Status']].append(item['Deals'])
+
+def count_len_stat(status):
+    """
+    a function that counts the number of values per key (the number of deals assigned to the person)
+    """
+    count = len(dict_status_deals.get(status))
+    return count
+
+# print(f"""
+# \033[4mTotal:\033[0m
+# Initial DD: {count_len_stat('Initial DD')}
+# Need 2nd Opinion: {count_len_stat('Need 2nd Opinion')}
+# Get Intro: {count_len_stat('Get Intro')}\n""")
+
+
+###########################################################
+
+# create a dictionary that contains the person, the status of their deals, and print out this overview
+d2 = [dict(zip(["Deals","Person", "Status"], item)) for item in data]
+
+dict_person_status = defaultdict(list)
+
+for item in d2:
+    dict_person_status[item['Person']].append(item['Status'])
+
+def count_status(person, status):
+    """
+    A function that counts the number of times a STATUS repeats itself in a list (value) assigned to a PERSON (key)
+    """
+    count = dict_person_status.get(person).count(status)
+    return count
+
+# print(f"""
+# \033[4mHrishi:\033[0m
+# Initial DD: {count_status('Hrishi', 'Initial DD')}
+# Need 2nd Opinion: {count_status('Hrishi', 'Need 2nd Opinion')}
+# Get Intro: {count_status('Hrishi', 'Get Intro')}\n
+# \033[4mMads:\033[0m
+# Initial DD: {count_status('mpedersen1@babson.edu', 'Initial DD')}
+# Need 2nd Opinion: {count_status('mpedersen1@babson.edu', 'Need 2nd Opinion')}
+# Get Intro: {count_status('mpedersen1@babson.edu', 'Get Intro')}\n
+# """)
+
+
+
+print(f"""
+\033[4mIndivivdual Pipelines\033[0m\n
+Hrishi:
+Total Deals: {count_len_dict('Hrishi')}
+Initial DD: {count_status('Hrishi', 'Initial DD')}
+Get Intro: {count_status('Hrishi', 'Get Intro')}
+Need 2nd Opinion: {count_status('Hrishi', 'Need 2nd Opinion')}
+Deals: {dict_person_deals.get('Hrishi')}\n
+Mads:
+Total Deals: {count_len_dict('mpedersen1@babson.edu')}
+Initial DD: {count_status('mpedersen1@babson.edu', 'Initial DD')}
+Get Intro: {count_status('mpedersen1@babson.edu', 'Get Intro')}
+Need 2nd Opinion: {count_status('mpedersen1@babson.edu', 'Need 2nd Opinion')}
+Deals: {dict_person_deals.get('mpedersen1@babson.edu')}\n
+""")
+
+
+###########################################################
+
+# Find the number of Initial DD's that are older than 48hrs:
+# create a dictionary that contains the person, the status of their deals, the date of the deal and finds "Initial DD" deals 
+# that are older than 48hrs and prints the total
+d3 = [dict(zip(["Deals","Person", "Status", "Date"], item)) for item in data]
+
+# create a function that filters for "Initial DD" only
+
+dict_deal_age = defaultdict(list)
+
+# create a function that calculate the age of a deal
+
+def date_extractor():
+    for item in d3:
+        dict_deal_age[item['Deals']].append(item['Date'])
+
+date_extractor()
+
+print(dict_deal_age)
