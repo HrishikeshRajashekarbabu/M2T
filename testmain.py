4bb6851df72a7363cc5b703218a10cce3ff3757f
Madskuz <112597537+Madskuz@users.noreply.github.com>
2022-12-05 23:29:57 -0500

    up

---
+++ testmain.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from collections import Counter
+
+apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIwNTEwMjMyMywidWlkIjozNzEwMjA4OSwiaWFkIjoiMjAyMi0xMi0wNVQyMjoxNzo1OS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQzNjU4NTEsInJnbiI6InVzZTEifQ.u0OkluQ6Ks7JvSVByLv8sjhkmkTgEN7QkTEIFoGfsZ0"
+apiUrl = "https://api.monday.com/v2"
+headers = {"Authorization" : apiKey}
+
+query2 = '{boards(limit:1) { name id description items { name column_values{title id type text } } } }'
+data = {'query' : query2}
+
+r = requests.post(url=apiUrl, json=data, headers=headers) # make request
+print(r.json())
+
+json_response = r.json
+
+counter = Counter([item['status'] for item in json_response])
+
+print(counter["Initial DD"])
